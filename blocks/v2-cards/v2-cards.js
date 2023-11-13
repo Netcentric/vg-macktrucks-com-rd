0d32139074db51b6f307cbdbd0321816c203ba9c
Syb Wartna <sybren.wartna@cognizant.com>
2023-11-13 16:42:07 +0100

    add variant without padding/background

--- blocks/v2-cards/v2-cards.js
+++ blocks/v2-cards/v2-cards.js
@@ -2,7 +2,7 @@ import { variantsClassesToBEM } from '../../scripts/common.js';
 
 export default async function decorate(block) {
   const blockName = 'v2-cards';
-  const variantClasses = ['gray-cards', 'horizontal'];
+  const variantClasses = ['no-background', 'gray-cards', 'horizontal'];
   variantsClassesToBEM(block.classList, variantClasses, blockName);
 
   const cardsItems = [...block.querySelectorAll(':scope > div')];
