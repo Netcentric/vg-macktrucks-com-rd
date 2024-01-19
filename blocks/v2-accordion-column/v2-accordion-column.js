90fb3de019c1d7e9cb651d4b4c0ef22298ad3bb7
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-01-19 17:00:13 +0100

    add a img placeholder & other style updates

--- blocks/v2-accordion-column/v2-accordion-column.js
+++ blocks/v2-accordion-column/v2-accordion-column.js
@@ -2,11 +2,10 @@ import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
 
 const CLASSES = {
   blockName: 'v2-accordion-column',
-  grayBackground: 'gray-background',
   left: 'left',
 };
 
-const { blockName, left, grayBackground } = CLASSES;
+const { blockName, left } = CLASSES;
 const variants = Object.values(CLASSES).splice(1);
 
 const addAccordionClass = (item) => {
@@ -26,26 +25,16 @@ export default function decorate(block) {
   const colGap = createElement('div', { classes: `${blockName}__accordion-gap` });
   const itemsContainer = createElement('div', { classes: `${blockName}__items-container` });
   const hasLeftClass = block.classList.contains(left); // accordion at left side
-  const hasGrayBgClass = block.classList.contains(grayBackground); // accordion with gray background
   /** @type {boolean} */
   const isLeftVariant = hasLeftClass
     || (!hasLeftClass && !!accordionItems[0].lastElementChild.querySelector('picture'));
-  // remove the gray variant and add it as section class
-  if (hasGrayBgClass) {
-    const section = 'section';
-    const sectionEl = block.closest(`.${section}`);
-    block.classList.remove(grayBackground);
-    sectionEl.classList.add(grayBackground);
-    variantsClassesToBEM(sectionEl.classList, variants, section);
-  }
   if (!hasLeftClass && isLeftVariant) block.classList.add(left);
   variantsClassesToBEM(block.classList, variants, blockName);
   header.parentElement.classList.add(`${blockName}__header-wrapper`);
   header.parentElement.parentElement.classList.add(`${blockName}__header-container`);
 
   // style the header as an h2 with red marker over it
-  header.classList.remove('h1', 'h2', 'h3', 'h4', 'h5', 'h6');
-  header.classList.add(`${blockName}__header`, 'h2', 'with-marker');
+  header.classList.add(`${blockName}__header`, 'with-marker');
 
   // is responsibility of the author to add the proper amount of images and text
   accordionItems.forEach((item, i) => {
@@ -66,8 +55,10 @@ export default function decorate(block) {
     colBtnTitle.prepend(item.querySelector(`.${blockName}__item-title`), arrowEl);
     colBtnTitle.onclick = () => {
       const active = accordionContainer.querySelector('.active');
+      const gapEl = accordionContainer.querySelector(`.${blockName}__accordion-gap`);
       if (active && active !== item) active.classList.remove('active');
       item.classList.toggle('active');
+      gapEl.classList.toggle('all-closed', !accordionContainer.querySelector('.active'));
     };
     item.prepend(colGap, colBtnTitle);
     itemsContainer.appendChild(item);
