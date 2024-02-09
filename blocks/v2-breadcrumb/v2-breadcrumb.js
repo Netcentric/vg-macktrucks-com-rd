89ca08f16945c5c55feeabc19d79a2222fdc8293
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-02-09 10:19:34 +0100

    comment update & a11y structure

--- blocks/v2-breadcrumb/v2-breadcrumb.js
+++ blocks/v2-breadcrumb/v2-breadcrumb.js
@@ -1,20 +1,21 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
-import { createElement } from '../../scripts/common.js';
+import { createElement, getTextLabel } from '../../scripts/common.js';
 
 const blockName = 'v2-breadcrumb';
 const sectionStatus = 'data-section-status';
+const breadcrumb = getTextLabel('breadcrumb');
 const homeText = {
-  home: 'home',
-  ellipsis: '...',
+  home: getTextLabel('home'),
+  ellipsis: '…', // unicode ellipsis
 };
 
-const removePathDash = (str) => str.replace(/-/g, ' ').toLowerCase();
+const formatText = (str) => str.replace(/-/g, ' ').toLowerCase();
 
 const getPadding = (elCompCSS) => parseInt(elCompCSS.getPropertyValue('padding-left'), 10)
       + parseInt(elCompCSS.getPropertyValue('padding-right'), 10);
 
 const getCrumbsWidth = (block) => {
-  const crumbs = block.querySelectorAll(`.${blockName}__crumb`);
+  const crumbs = block.querySelectorAll(`.${blockName}__crumb-item`);
   return [...crumbs].reduce((acc, item) => {
     const itemCompCSS = window.getComputedStyle(item);
     return acc + parseInt(itemCompCSS.getPropertyValue('width'), 10);
@@ -29,18 +30,18 @@ const getBlockWidth = (block) => {
   return blockWidth - padding;
 };
 
-const areCrumbsFit = (block) => getCrumbsWidth(block) < getBlockWidth(block);
-
+const fitting = (block) => getCrumbsWidth(block) < getBlockWidth(block);
 export default function decorate(block) {
   const cfg = readBlockConfig(block);
   const hasPath = cfg && Object.hasOwn(cfg, 'path');
-  const url = new URL(window.location.href);
-
-  if (hasPath) url.pathname = cfg.path;
-
-  const path = url.pathname.split('/').filter(Boolean);
+  const url = hasPath ? cfg.path : window.location.pathname;
+  const path = url.split('/').filter(Boolean);
+  const nav = createElement('nav', { classes: [`${blockName}__crumb-nav`] });
+  const ul = createElement('ul', { classes: [`${blockName}__crumb-list`] });
   const crumbs = path.map((_, i) => {
-    const crumbProps = { 'data-content': removePathDash(path[i]) };
+    const liEl = createElement('li', { classes: [`${blockName}__crumb-item`] });
+    const content = formatText(path[i]);
+    const crumbProps = { 'data-content': content };
     const crumbClasses = [`${blockName}__crumb`];
     if (i !== path.length - 1) {
       crumbProps.href = `/${path.slice(0, i + 1).join('/')}/`;
@@ -49,33 +50,40 @@ export default function decorate(block) {
       crumbProps['aria-current'] = 'page';
     }
     const crumb = createElement('a', { classes: crumbClasses, props: crumbProps });
-    crumb.textContent = removePathDash(path[i]);
-    return crumb;
+    crumb.textContent = content;
+    liEl.append(crumb);
+    return liEl;
   });
+  const homeItem = createElement('li', { classes: [`${blockName}__crumb-item`] });
   const homeEl = createElement('a', {
     classes: [`${blockName}__crumb`, `${blockName}__crumb--home`],
     props: { href: '/' },
   });
 
   homeEl.textContent = homeText.home;
-  crumbs.unshift(homeEl);
+  homeItem.append(homeEl);
+  crumbs.unshift(homeItem);
+  ul.append(...crumbs);
+  nav.append(ul);
   block.textContent = '';
-  block.append(...crumbs);
+  block.append(nav);
   block.parentElement.classList.add('full-width');
-  block.setAttribute('aria-label', 'Breadcrumb');
+  block.setAttribute('aria-label', breadcrumb);
 
-  const CheckCrumbsFits = () => {
+  const checkCrumbsFits = () => {
     // 1st check if home fits, if not it become an ellipsis
-    if (!areCrumbsFit(block) && crumbs.length > 2) homeEl.textContent = homeText.ellipsis;
+    if (!fitting(block) && crumbs.length > 2) homeEl.textContent = homeText.ellipsis;
     // if still doesn't fit, remove active crumb
-    if (!areCrumbsFit(block)) {
-      crumbs.at(-1).textContent = '';
+    if (!fitting(block)) {
+      crumbs.at(-1).firstElementChild.textContent = '';
+      crumbs.at(-1).classList.add(`${blockName}__crumb-item--hidden`);
     }
     // if it still doesn't fit again, remove the crumbs from the middle
-    if (!areCrumbsFit(block)) {
+    if (!fitting(block)) {
       let i = 1;
-      while (i < crumbs.length - 2 && !areCrumbsFit(block)) {
-        crumbs[i].textContent = '';
+      while (i < crumbs.length - 2 && !fitting(block)) {
+        crumbs[i].firstElementChild.textContent = '';
+        crumbs[i].classList.add(`${blockName}__crumb-item--hidden`);
         i += 1;
       }
     }
@@ -87,10 +95,13 @@ export default function decorate(block) {
       // add again the content from each item and check if it fits again or not
       homeEl.textContent = homeText.home;
       crumbs.forEach((crumb, i) => {
-        if (i === 0) return;
-        crumb.textContent = crumb.dataset.content;
+        const link = crumb.firstElementChild;
+        if (i > 0) {
+          crumb.classList.remove(`${blockName}__crumb-item--hidden`);
+          link.textContent = link.dataset.content;
+        }
       });
-      CheckCrumbsFits();
+      checkCrumbsFits();
     });
   });
 
